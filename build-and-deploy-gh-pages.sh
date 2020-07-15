1c58379f8d0707136b4ae2d3e838ae29ca86b531
hugogomess <hugogomes02@gmail.com>
2020-07-15 07:39:52 -0300

    add gh-pages build script

--- build-and-deploy-gh-pages.sh
+++ build-and-deploy-gh-pages.sh
@@ -2,8 +2,10 @@
 
 cp -r ./public ./build;
 git checkout master;
-
-
-
+mv ./build/* ./;
+mv ./build/fonts/* ./fonts;
 rm ./build;
+git add . && git commit -m "build for gh-pages" && git push origin master;
+git checkout dev;
+
 exit
