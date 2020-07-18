62da00bb46566751182c6acb0baf2bcc63c0b277
hugogomess <hugogomes02@gmail.com>
2020-07-17 23:27:40 -0300

    add twitter, hackthebox and tryhackme

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -6,11 +6,17 @@
     <h2>Social media</h2>
     <p>
       You can find me on
-      <a :href="config.links.social.github" target="_blank">Github</a> and
+      <a :href="config.links.social.github" target="_blank">Github</a>, 
+      <a :href="config.links.social.twitter" target="_blank">Twitter</a> and
       <a :href="config.links.social.linkedin" target="_blank">Linkedin</a>.
     </p>
 
     <h2>Email: <a :href="config.links.social.email" target="_blank">{{ email }}</a></h2>
+
+    <p>
+      <script src="https://www.hackthebox.eu/badge/323852"></script>
+      <script src="https://tryhackme.com/badge/116953"></script>
+    </p>
   </section>
 </template>
 
