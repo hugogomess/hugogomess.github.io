993d7807ed5b63ea1279f9f498a88ccfbcaae272
hugogomess <hugogomes02@gmail.com>
2020-07-19 08:12:14 -0300

    remove tryhackme badge

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -15,12 +15,10 @@
 
     <br/>
 
+    <h2>Hack The Box</h2>
     <p>
       <a class="no-border" href="https://www.hackthebox.eu/home/users/profile/323852" target="_blank">
         <img src="http://www.hackthebox.eu/badge/image/323852" alt="Hack The Box">
-      </a>
-      <a class="no-border" href="https://tryhackme.com/p/0xkaneki" target="_blank">
-        <img src="https://tryhackme-badges.s3.amazonaws.com/0xkaneki.png" alt="TryHackMe">
       </a>
     </p>
   </section>
