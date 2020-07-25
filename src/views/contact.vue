77618eb685459d3537ec4bd7e26ec8425367f63b
hugogomess <hugogomes02@gmail.com>
2020-07-25 12:56:44 -0300

    add discord

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,24 +1,38 @@
 <template>
   <section class="section">
-    <router-link :to="{ name: 'home'}" id="back-link">Back</router-link>
+    <router-link :to="{ name: 'home' }" id="back-link">Back</router-link>
     <h1 class="title-section">Contact</h1>
 
     <h2>Social media</h2>
     <p>
       You can find me on
-      <a :href="config.links.social.github" target="_blank">Github</a>, 
+      <a :href="config.links.social.github" target="_blank">Github</a>,
       <a :href="config.links.social.twitter" target="_blank">Twitter</a> and
       <a :href="config.links.social.linkedin" target="_blank">Linkedin</a>.
     </p>
 
-    <h2>Email: <a :href="config.links.social.email" target="_blank">{{ email }}</a></h2>
+    <h2>
+      Email:
+      <a :href="config.links.social.email" target="_blank">{{ email }}</a>
+    </h2>
 
-    <br/>
+    <br />
+
+    <h2>Discord: {{ discord }}</h2>
+
+    <br />
 
     <h2>Hack The Box</h2>
     <p>
-      <a class="no-border" href="https://www.hackthebox.eu/home/users/profile/323852" target="_blank">
-        <img src="http://www.hackthebox.eu/badge/image/323852" alt="Hack The Box">
+      <a
+        class="no-border"
+        href="https://www.hackthebox.eu/home/users/profile/323852"
+        target="_blank"
+      >
+        <img
+          src="http://www.hackthebox.eu/badge/image/323852"
+          alt="Hack The Box"
+        />
       </a>
     </p>
   </section>
@@ -32,11 +46,11 @@ export default Vue.extend({
   data() {
     return {
       config,
-      email: "hugogomes02@gmail.com"
+      email: "hugogomes02@gmail.com",
+      discord: "0xkaneki#0487",
     };
-  }
+  },
 });
 </script>
 
-<style>
-</style>
+<style></style>
