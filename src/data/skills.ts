41a1d4c3f7ab4410e7e45fb3524258c426a2d6ae
hugogomess <hugogomes02@gmail.com>
2020-07-26 12:56:15 -0300

    add skill set table

---
+++ src/data/skills.ts
@@ -0,0 +1,25 @@
+/*
+tags:
+advanced
+beginner
+interested
+intermediary
+work-experience
+*/
+
+export default [
+  {
+    name: 'Languages',
+    items: [
+      { name: 'Javascript', tags: ['advanced', 'work-experience', 'beginner', 'interested', 'intermediary'] },
+      { name: 'Typescript', tags: ['advanced', 'work-experience'] },
+    ],
+  },
+  {
+    name: 'Back End',
+    items: [
+      { name: 'Node.JS', tags: ['advanced', 'work-experience'] },
+      { name: 'REST APIs', tags: ['advanced', 'work-experience'] },
+    ],
+  },
+];
--- src/views/skills.vue
+++ src/views/skills.vue
@@ -3,36 +3,41 @@
     <router-link :to="{ name: 'home' }" id="back-link">Back</router-link>
     <h1 class="title-section">Skill Set</h1>
 
-    <p>Here you can find my skill set</p>
+    <p>My skill set separated by different categories</p>
 
-    <!-- <div class="container">
-    <header> <h3 class="tcaption"> {skill.name |> rs} </h3> </header>
-    <table class="table">
-      <thead>
-        <tr>
-          <th class="theader"> {"Name" |> rs} </th>
-          <th class="theader"> {"Tags" |> rs} </th>
-        </tr>
-      </thead>
-      <tbody>
-        {List.mapi(
-           (key, attr) => {<Item key={key |> string_of_int} attr />},
-           skill.attribs,
-         )
-         |> Array.of_list
-         |> ReasonReact.array}
-      </tbody>
-    </table>
-    </div>-->
+    <div v-for="(skill, index) in skills" :key="index" class="skill-container">
+      <header>
+        <h3 class="skill-tcaption">{{skill.name}}</h3>
+      </header>
+      <table class="skill-table">
+        <thead>
+          <tr>
+            <th class="skill-theader">Name</th>
+            <th class="skill-theader">Tags</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, key) in skill.items" :key="key">
+            <td class="skill-tfixed">{{item.name}}</td>
+            <td class="skill-tdata">
+              <div v-for="(tag, key) in item.tags" :key="key" :class="tag +' skill-tag'">{{tag}}</div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </section>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import skills from "../data/skills";
 
 export default Vue.extend({
   data() {
-    return {};
+    return {
+      skills,
+    };
   },
 });
 </script>
@@ -41,5 +46,75 @@ export default Vue.extend({
 ul li a {
   margin: 0 20px 0 0;
   line-height: 30px;
+}
+
+.skill-container {
+  background-color: rgb(64, 61, 88);
+  border: radius 16px;
+  color: white;
+  height: 100pct;
+}
+
+.skill-tcaption {
+  font-size: 1.4rem;
+  font-weight: bold;
+  line-height: 1.25abs;
+  letter-spacing: 1px;
+  margin-bottom: 4px;
+  margin-left: 0px;
+  margin-right: 0px;
+  margin-top: 16px;
+  text-align: center;
+}
+
+.skill-table {
+  padding: 16px;
+  width: 100pct;
+}
+
+.skill-theader {
+  display: none;
+}
+
+.skill-tdata {
+  border-bottom: 1px solid rgb(81, 78, 105);
+  padding: 2px;
+}
+
+.skill-tfixed {
+  border-bottom: 1px solid rgb(81, 78, 105);
+  padding: 2px;
+  width: 212px;
+}
+
+.skill-tag {
+  border-radius: 4px;
+  color: rgb(55, 53, 47);
+  display: inline-block;
+  font-size: 0.8rem;
+  line-height: 120pct;
+  margin-right: 4px;
+  /* media: ("(max-width: 960px)", [marginBottom(px(4))]), */
+  padding: 2px;
+}
+
+.beginner {
+  background-color: rgb(249, 255, 163);
+}
+
+.advanced {
+  background-color: rgb(255, 129, 142);
+}
+
+.interested {
+  background-color: rgb(145, 215, 255);
+}
+
+.intermediary {
+  background-color: rgb(255, 159, 91);
+}
+
+.work-experience {
+  background-color: rgb(188, 255, 193);
 }
 </style>
