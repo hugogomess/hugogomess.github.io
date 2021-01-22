6bb594b19de27e34bb19d7a4703b74ae56a73404
hugogomess <hugogomes02@gmail.com>
2021-01-22 11:51:54 -0300

    chore: update htb link

--- src/data/social.ts
+++ src/data/social.ts
@@ -4,7 +4,7 @@ export default {
     github: 'https://github.com/hugogomess',
     medium: 'https://medium.com/@0xkaneki',
     twitter: 'https://twitter.com/0xkaneki',
-    htb: 'https://www.hackthebox.eu/home/users/profile/323852',
+    htb: 'https://app.hackthebox.eu/profile/323852',
     thm: 'https://tryhackme.com/p/0xkaneki',
   },
 };
