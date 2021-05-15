5e5559884e1f01802cb04745672c785beef451b2
sukenn <hugogomes02@gmail.com>
2021-05-15 20:38:27 -0300

    update data

--- src/data/blog-posts.ts
+++ src/data/blog-posts.ts
@@ -3,6 +3,7 @@ export default [
     title: "THM Bounty Hacker — Write-up",
     url: "/cowboyhacker",
     redirectTo:
-      "https://medium.com/@sukennzx/thm-bounty-hacker-write-up-27e04fcb7"
+      "https://medium.com/@sukennzx/thm-bounty-hacker-write-up-27e04fcb7",
+    tags: ["ctf", "write-up", "thm"]
   }
 ];
---
+++ src/data/contact.ts
@@ -0,0 +1,11 @@
+export default {
+  email: "sukennzx@gmail.com",
+  links: {
+    htb: "https://app.hackthebox.eu/profile/323852",
+    twitter: "https://twitter.com/sukennzx",
+    github: "https://github.com/sukenn",
+    telegram: "https://t.me/sukennzx",
+    thm: "https://tryhackme.com/p/sukenn",
+    medium: "https://medium.com/@sukennzx"
+  }
+};
